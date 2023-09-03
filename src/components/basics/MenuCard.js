0cf2b8a98308f6e47247ada4f3996ad6ee464dbe
Ashish kumar pathak <ashish-kumar.pathak@s2021.tu-chemnitz.de>
2023-09-03 11:42:46 +0200

    first commit

---
+++ src/components/basics/MenuCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+    console.log(menuData)
+  return (
+    <div>
+
+    {menuData.map((currentElem) => {
+        return (
+            <div>
+            <div className="card-container">
+            <div className="card ">
+            <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle">Breakfast</span>
+            <h2 className="card-title"> Maggi </h2>
+            <span className="card-description subtle">
+            I love Maggi realy oo yues Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Blanditiis, at consectetur totam voluptatibus
+            quibusdam iusto. Accusamus quas, soluta ipsam autem eius
+            necessitatibus fugiat in
+            </span>
+            <div className="card-read">Read</div>
+            </div>
+            {/* <img src={image} alt="images" className="card-media"></img> */}
+            <span className="card-tag_subtle">Order Now</span>
+            </div>
+            </div>
+            </div>
+        )
+    })}
+
+
+ 
+    </div>
+  )
+}
+
+export default MenuCard
--- src/components/basics/Restaurant.js
+++ src/components/basics/Restaurant.js
@@ -1,38 +1,16 @@
 import React  , {useState } from 'react'
 import "./style.css"
-
-import Menu from "./menuApi.js" ;
+import Menu from "./menudata" 
+import MenuCard from "./MenuCard"
 
 const Restaurant = () => {
-
-
     const [menuData , setMenuData]  = useState(Menu)
-
+      
     console.log(menuData);
   return (
     <div>
 
-
-    <div className="card-container">
-    <div className="card ">
-    <div className="card-body">
-    <span className="card-number card-circle subtle">1</span>
-    <span className="card-author subtle">Breakfast</span>
-    <h2 className="card-title"> Maggi </h2>
-    <span className="card-description subtle">
-    I love Maggi realy oo yues Lorem ipsum dolor sit amet consectetur
-    adipisicing elit. Blanditiis, at consectetur totam voluptatibus
-    quibusdam iusto. Accusamus quas, soluta ipsam autem eius
-    necessitatibus fugiat in
-    </span>
-    <div className="card-read">Read</div>
-    </div>
-    {/* <img src={image} alt="images" className="card-media"></img> */}
-    <span className="card-tag_subtle">Order Now</span>
-    </div>
-    </div>
-
-
+    < MenuCard  menuData = {menuData}  / >
     </div>
   )
 }
